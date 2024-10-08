9f58837b422ab2ff8c71dc19d54a73125591619a
David Heidema <david.heidema@genmills.com>
2024-10-08 03:12:21 +0000

    Refactor to use a config value file to user-level inputs

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # GamingAlerts
 Send texts to friends when you are gaming
+
+Carrier Mapping:
+Some carriers require a country code at the beginning of the entered phone number. 
+Verizon and Pure Talk do not require the country code, while Mint Mobile requires the country code.
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import pytz
 import requests
 import os
 import load_dotenv
+import yaml
 
 load_dotenv.load_dotenv() 
 
@@ -28,6 +29,16 @@ carrier_gateway_map = {
     "Cricket": "sms.cricketwireless.net",
     "US_Cellular": "email.uscc.net"
 }
+
+
+
+def load_user_config():
+    with open("userConfig.yaml") as stream:
+        try: 
+            return yaml.safe_load(stream)
+        except:
+            print(exec)
+       
 
 
 ###https://discord.com/developers/docs/topics/oauth2#bot-users
@@ -90,16 +101,24 @@ def get_steam_logoff(summary_data):
     player = summary_data['response']['players'][0]
     return player['lastlogoff']
     
-def sendText(currGame, recipient_number, recipient_carrier):
+def sendText(currGame, gamer_details):
 
+    gamer_name = gamer_details.get('gamerName') 
+    recipient_carrier = gamer_details.get('recipientCarrier')
+    recipient_number = gamer_details.get('recipientPhoneNumber')
+    recipient_name = gamer_details.get('recipientName')
+
+    if gamer_name is None or recipient_carrier is None or recipient_number is None or recipient_name is None:
+        sys.exit('Malformed data entered in the userConfig file.')
+    
     recipient_carrier_gateway = carrier_gateway_map.get(recipient_carrier) 
     num = f'{recipient_number}@{recipient_carrier_gateway}'
 
     now = pytz.timezone('America/New_York')
     utc_Ny = datetime.datetime.now(now)
-    format_time= str(utc_Ny.strftime('%H:%M:%S'))
+    format_time= str(utc_Ny.strftime('%H:%M'))
  
-    text = f'\nDavid is playing {currGame}! At {format_time}'
+    text = f'\n{gamer_name} is playing {currGame}! \nGaming session started at {format_time}'
     subject = 'GAMING ALERT!'
     ## ensure the message is ASCII before using SMTP protocol
     message = 'Subject: {}\n\n{}'.format(subject, text).encode('ascii', 'replace').decode('ascii').strip().replace("?","")
@@ -107,22 +126,17 @@ def sendText(currGame, recipient_number, recipient_carrier):
         s = smtplib.SMTP('smtp.gmail.com',587,timeout=3000)
         s.starttls()
         s.login(email_to_send_from, app_password_from_email)
-        
         s.sendmail(email_to_send_from, num, message)
+        print("Successfully texted", recipient_name)
         
     except Exception as e:
         sys.exit( "mail failed- %s", e ) # give an error message
     finally:
         s.quit() 
-
-
+    
 if __name__ == "__main__":
-   
-    if(len(sys.argv)) < 3:  
-        print("Invalid input: Please enter your friend's phone number as arg 1, and their mobile carrier as arg2")
-        sys.exit()
-    target_phone_number = sys.argv[1]
-    target_carrier = sys.argv[2]
+
+    user_details = load_user_config()
 
     message_status = "NA"
     start_gaming_time = int(time.time())
@@ -133,11 +147,11 @@ if __name__ == "__main__":
         steam_user_id = get_steam_user_id(discord_user_token)
         while(True):
             user_data = get_steam_user_summary(steam_user_id)
+            if start_gaming_time < get_steam_logoff(user_data):
+                sys.exit("Gamer is no longer online, goodbye.")
             current_steam_game = get_curr_steam_game(user_data)
             if current_steam_game != "0" and current_steam_game != message_status:
-                sendText(current_steam_game, target_phone_number, target_carrier)
+                sendText(current_steam_game, user_details)
                 message_status = current_steam_game
             print(message_status)
-            time.sleep(200)
-            if start_gaming_time < get_steam_logoff(user_data):
-                break
+            time.sleep(60)
